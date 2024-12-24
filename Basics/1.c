6f2bde34f410beb5b995bd517f4d9774c9c6ce43
Kavyan Nayak <kavyannayak104@gmail.com>
2024-12-24 20:13:00 +0530

    Basics

---
+++ Basics/1.c
@@ -0,0 +1,15 @@
+/*
+
+                Name: Mr. Kavyan Nayak
+                Date :- 24-12-2024
+                obj :- Print the Hello world 
+*/
+
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    clrscr();
+    printf("Hello World");
+    getch();
+}
