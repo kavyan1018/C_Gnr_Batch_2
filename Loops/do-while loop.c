b740160612559b2e08fb2b1129f3b26b4d076ab8
Kavyan Nayak <kavyannayak104@gmail.com>
2025-01-21 18:41:56 +0530

    do-While loop

---
+++ Loops/do-while loop.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+    int i;
+
+    // intialization
+    i = 1;
+    do
+    {
+        /* code */
+        printf("Hello World\n");
+        i++;
+         /* condition */
+    }while (i<=5);
+}
