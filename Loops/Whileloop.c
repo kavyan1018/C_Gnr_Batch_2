20ec5e0021ecd3f22c901608ae060925f9ad0672
Kavyan Nayak <kavyannayak104@gmail.com>
2025-01-21 18:41:46 +0530

    While loop

---
+++ Loops/Whileloop.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+    int i;
+
+    // intialization
+    i = 1;
+    /* condition */
+    while (i<=5)
+    {
+        /* code */
+        printf("Hello World\n");
+        i++;
+    }
+}
