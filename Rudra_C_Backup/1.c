374faf41c7d102e9dfabf63b9914f3161661897c
Kavyan Nayak <kavyannayak104@gmail.com>
2025-02-26 11:27:11 +0530

    Rudra_Backup

---
+++ Rudra_C_Backup/1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+    // addition of 2 numebrs 
+    int a, b, sum;
+
+    a = 10;
+    b = 20;
+
+    sum = a + b;
+
+    printf("\nAddition is %d",sum);
+
+    printf("\nAddition is %d",a + b);
+
+
+}
---
+++ Rudra_C_Backup/IF-ELSE/1.c
@@ -0,0 +1,24 @@
+// syntax of if-else
+/*
+if (condition)
+{
+    // code
+}
+else
+{
+    // code
+}
+
+*/
+
+
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+    int a,b;
+
+    printf("Enter the ");
+
+}
