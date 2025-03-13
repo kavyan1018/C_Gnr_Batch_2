4f1d987c6a6c40d811c8e6708cf79240bf7230ad
Kavyan Nayak <kavyannayak104@gmail.com>
2025-03-13 12:11:37 +0530

    NR - NA

--- Function/NR - NA.c
+++ Function/NR - NA.c
@@ -1,33 +1,90 @@
-#include<stdio.h>
-#include<conio.h>
+// #include<stdio.h>
+// #include<conio.h>
 
-// Define function prototype
+// // Define function prototype
+// int addition();
+// void main()
+// {
+//     // Call the addition function
+//     addition();
+// }
+
+// int addition()
+// {
+//     // Declare variables
+//     int a, b, sum;
+
+//     // clrscr();
+
+//     // Prompt user for input
+//     printf("Enter two numbers: ");
+//     scanf("%d %d", &a, &b);
+
+//     // Calculate sum
+//     sum = a + b;
+
+//     // Display result
+//     printf("Sum = %d\n", sum);
+//     // Wait for user to press a key before exiting
+
+//     // getch();
+
+//     // Return to the calling function
+// }
+
+#include <stdio.h>
+#include <conio.h>
+
 int addition();
-void main()
+int Subtraction();
+int main()
 {
-    // Call the addition function
-    addition();
+    int choice;
+
+    printf("1. Addition\n");
+    printf("2. Subtraction\n");
+    printf("3. Multiplication\n");
+    printf("4. Division\n");
+    printf("5. Exit\n");
+    printf("Enter Your Choice :");
+    scanf("%d", &choice);
+
+    switch (choice)
+    {
+    case 1:
+        addition();
+        break;
+
+    case 2:
+        Subtraction();
+        break;
+    
+    default:
+        printf("Invalid choice\n");
+        break;
+    }
 }
 
 int addition()
 {
-    // Declare variables
     int a, b, sum;
 
-    // clrscr();
-
-    // Prompt user for input
     printf("Enter two numbers: ");
     scanf("%d %d", &a, &b);
 
-    // Calculate sum
     sum = a + b;
 
-    // Display result
     printf("Sum = %d\n", sum);
-    // Wait for user to press a key before exiting
+}
 
-    // getch();
+int Subtraction()
+{
+    int a, b, sum;
 
-    // Return to the calling function
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    sum = a - b;
+
+    printf("Sum = %d\n", sum);
 }
