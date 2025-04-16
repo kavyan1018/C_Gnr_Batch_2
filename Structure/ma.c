bdb72c9516b2bbc84f30ba43ad46244e4e931561
Kavyan Nayak <kavyannayak104@gmail.com>
2025-04-16 15:17:32 +0530

    Update

--- Structure/ma.c
+++ Structure/ma.c
@@ -19,12 +19,11 @@ void main()
 
     struct student a[n];
 
-    
     // using loops
-    
+
     for (int i = 0; i < n; i++)
     {
-        printf("---Enter the Student Details %d---\n",i +1);
+        printf("---Enter the Student Details %d---\n", i + 1);
 
         printf("Enter Roll No: ");
         scanf("%d", &a[i].Roll_No);
@@ -43,4 +42,48 @@ void main()
         printf("Name: %s\n", a[i].Name);
         printf("Marks: %.2f\n\n", a[i].Marks);
     }
+
+    // Update the Student data by Roll No
+
+    int roll_no, found = 0;
+
+    printf("\nEnter Roll No to update: ");
+    scanf("%d", &roll_no);
+
+    for (int i = 0; i < n; i++)
+    {
+
+        if (a[i].Roll_No == roll_no)
+        {
+            found = 1;
+
+            printf("---Update the Student Details %d---\n", roll_no);
+
+            printf("Enter Name: ");
+            scanf("%s", a[i].Name);
+
+            printf("Enter Marks: ");
+            scanf("%f", &a[i].Marks);
+
+            printf("\n---Student Details Updated Successful---\n");
+        }
+    }
+
+    if (!found)
+    {
+        printf("\nRoll No %d not found.\n", roll_no);
+    }
+
+
+    // Display the updated student details
+
+
+    printf("\n---Updated Student Details---\n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("Roll No: %d\n", a[i].Roll_No);
+        printf("Name: %s\n", a[i].Name);
+        printf("Marks: %.2f\n\n", a[i].Marks);
+    }
+
 }
