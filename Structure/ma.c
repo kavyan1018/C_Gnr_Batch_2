e32c46b32f249e1cc4fc29393645c354019fbe90
Kavyan Nayak <kavyannayak104@gmail.com>
2025-04-16 11:58:21 +0530

    Using Loops

--- Structure/ma.c
+++ Structure/ma.c
@@ -1,8 +1,9 @@
-#include<stdio.h>
-#include<conio.h>
+#include <stdio.h>
+#include <conio.h>
 
 // structure definition
-struct student{
+struct student
+{
 
     int Roll_No;
     char Name[20];
@@ -11,23 +12,35 @@ struct student{
 
 void main()
 {
-    struct student a;
 
-    printf("---Enter the Student Details---\n");
+    int n;
+    printf("Eneter the number of students: ");
+    scanf("%d", &n);
 
-    printf("Enter Roll No: ");
-    scanf("%d", & a.Roll_No);
+    struct student a[n];
 
-    printf("Enter Name: ");
-    scanf("%s", a.Name);
+    
+    // using loops
+    
+    for (int i = 0; i < n; i++)
+    {
+        printf("---Enter the Student Details %d---\n",i +1);
 
-    printf("Enter Marks: ");
-    scanf("%f", & a.Marks);
+        printf("Enter Roll No: ");
+        scanf("%d", &a[i].Roll_No);
 
+        printf("Enter Name: ");
+        scanf("%s", a[i].Name);
+
+        printf("Enter Marks: ");
+        scanf("%f", &a[i].Marks);
+    }
 
     printf("\n---Student Details---\n");
-    printf("Roll No: %d\n", a.Roll_No);
-    printf("Name: %s\n", a.Name);
-    printf("Marks: %.2f\n", a.Marks);
-
+    for (int i = 0; i < n; i++)
+    {
+        printf("Roll No: %d\n", a[i].Roll_No);
+        printf("Name: %s\n", a[i].Name);
+        printf("Marks: %.2f\n\n", a[i].Marks);
+    }
 }
