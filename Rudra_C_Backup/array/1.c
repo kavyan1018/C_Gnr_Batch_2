5f8da1b3f3774f6a8bce2a1f916674b3dfb22dda
Kavyan Nayak <kavyannayak104@gmail.com>
2025-04-24 14:51:50 +0530

    array

--- Rudra_C_Backup/array/1.c
+++ Rudra_C_Backup/array/1.c
@@ -3,30 +3,48 @@
 
 void main()
 {
-    // 1 - way to declare an array
-    int a[5] = {1, 2, 3, 4, 5};
-    int i , sum = 0;
+    int a[5], i;
 
-    // printf("a[1] = %d\n", a[1]);
-    // printf("a[2] = %d\n", a[4]);
-    // printf("a[3] = %d\n", a[2]);
-    // printf("a[4] = %d\n", a[3]);
-    // printf("a[5] = %d\n", a[4]);
+    printf("Enter 1st element of array: ");
+    scanf("%d", &a[0]);
 
+    printf("Enter 2nd element of array: ");
+    scanf("%d", &a[1]);
+
+    printf("Enter 3rd element of array: ");
+    scanf("%d", &a[2]);
+
+
+    printf("Enter 4th element of array: ");
+    scanf("%d", &a[3]);
+
+    printf("Enter 5th element of array: ");
+    scanf("%d", &a[4]);
+
+
+
+    printf("The elements of array are: ");
+    printf("%d ", a[0]);
+    printf("%d ", a[1]);
+    printf("%d ", a[2]);
+    printf("%d ", a[3]);
+    printf("%d ", a[4]);
+    
+
+
+
+    // using loop
+
+    // printf("Enter 5 elements of array: ");
 
     // for (i = 0; i < 5; i++)
     // {
-    //     printf("a[%d]    = %d\n", i, a[i]);
+    //     scanf("%d", &a[i]);
     // }
-
-
-    for (i = 0; i < 5; i++)
-    {
-        printf("a[%d]    = %d\n", i, a[i]);
-        sum += a[i];
-    }
-
-    printf("Sum of array = %d\n", sum);
-    // printf("Sum of array = %d\n", sum);
-
+    
+    // printf("The elements of array are: ");
+    // for (i = 0; i < 5; i++)
+    // {
+    //     printf("%d ", a[i]);
+    // }
 }
